e21ebbc56a83d933518be0d2e5550de2ff6d5a93
hexxal36 <hexxal360@gmail.com>
2020-04-19 17:13:29 +0300

    Added Sendgrid config.

---
+++ VacationFinder.Common/EmailConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace VacationFinder.Common
+{
+    public static class EmailConstants
+    {
+        public const string SendGridApiKey = ""; // Your Api Key Here
+
+        public const string Subject = "Your order has been accepted";
+
+        public const string From = ""; // Your Email Here
+
+        public const string Body = "To confirm your information and continue the order please call 12345678";
+    }
+}
